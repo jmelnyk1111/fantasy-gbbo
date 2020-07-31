0786f45045571e7428ee6c26a82659a5037667ef
Jared Melnyk <jaredmelnyk@Jareds-MacBook-Air.local>
2020-07-30 22:40:26 -0500

    added episode scoring array and started table layout of teams page

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 app.set("view engine", "ejs");
 
 const mongoose = require("mongoose");
@@ -16,18 +19,39 @@ conn.on('connected', () => {
     console.log("Mongoose is connected!");
 });
 
-//conn.once('open', function(){});
 
 let teamSchema = new mongoose.Schema({
     name: String,
     slogan: String,
+    score: Object,
 });
 
 let Team = mongoose.model("Team", teamSchema);
 
+// let teamTurtles = new Team({
+//     name:"Meryl Streeper 2", 
+//     slogan:"Somebody come geet er! NOW",
+//     score:{
+//         "ep1":6,
+//         "ep2":12.5,
+//         "ep3":4,
+//         "ep4":6,
+//         "ep5":12.5,
+//         "ep6":4,
+//         "ep7":6,
+//         "ep8":12.5,
+//         "ep9":4,
+//         "ep10":6,
+//     }
+// });
+
+// teamTurtles.save(function (err, team) {
+//     if (err) return console.error(err);
+//     console.log(team.name + " saved to Teams collection");
+// });
+
 app.get("/", function(req, res) {
     res.redirect("/teams");
-    //res.send("you're here");
 });
 
 app.get("/teams", function(req, res) {
@@ -45,7 +69,19 @@ app.get("/teams", function(req, res) {
 app.post("/teams", function(req, res) {
     let newTeam = {
         name: req.body.name,
-        slogan: req.body.slogan
+        slogan: req.body.slogan,
+        score:{
+            "ep1":0,
+            "ep2":0,
+            "ep3":0,
+            "ep4":0,
+            "ep5":0,
+            "ep6":0,
+            "ep7":0,
+            "ep8":0,
+            "ep9":0,
+            "ep10":0,
+        }
     };
 
     Team.create(newTeam, function(err, newlyCreated){
